a62042ade7c7fe97e46b628539af73cf873500d4
neacsiuandrei <andrey.andreyQ@yahoo.com>
2024-03-13 15:10:14 +0200

    first commit

---
+++ src/main/java/com/spring/training/SpringDiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiTrainingApplication.class, args);
+	}
+
+}
